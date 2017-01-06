c43f63152a64fff4eedd9be7bca09c2f6513e3de
françois GUILLEM <francois.guillem@rte-france.com>
2017-01-06 10:47:03 +0100

    More functions to work with lists of controls

--- R/controlsUtils.R
+++ R/controlsUtils.R
@@ -14,7 +14,7 @@ getControlDesc <- function(controls) {
  types <- c()
  groupLevel <- c()
 
- .getControlDesc <- function(x, name = "", level = 0) {
+ getControlDescRecursive <- function(x, name = "", level = 0) {
    if (is.function(x)) {
      value <- list(attr(x, "value"))
      inputNames <<- append(inputNames, name)
@@ -22,9 +22,9 @@ getControlDesc <- function(controls) {
      types <<- append(types, attr(x, "type"))
      groupLevel <<- append(groupLevel, level)
    }
-   else mapply(.getControlDesc, x=x, name = names(x), level = level + 1)
+   else mapply(getControlDescRecursive, x=x, name = names(x), level = level + 1)
  }
- .getControlDesc(controls)
+ getControlDescRecursive(controls)
 
  data.frame(
    name = inputNames,
@@ -34,19 +34,77 @@ getControlDesc <- function(controls) {
  )
 }
 
-# Internal function that filters a list of controls given a vector of names
-filterControls <- function(controls, names) {
-  .filterControls <- function(x) {
+# Internal function that filters a list of controls given a vector of names.
+# If drop = TRUE, controls whose name is in "names" are removed, else they
+# are kept and all other controls are removed.
+filterControls <- function(controls, names, drop = FALSE) {
+  filterControlsRecursive <- function(x) {
     for (n in names(x)) {
       if (is.list(x[[n]])) {
-        x[[n]] <- .filterControls(x[[n]])
+        x[[n]] <- filterControlsRecursive(x[[n]])
         if (length(x[[n]]) == 0) x[[n]] <- NULL
       } else {
-        if (!n %in% names) x[[n]] <- NULL
+        if (!n %in% names & !drop) x[[n]] <- NULL
+        if (n %in% names & drop) x[[n]] <- NULL
       }
     }
     return(x)
   }
 
-  .filterControls(controls)
+  filterControlsRecursive(controls)
+}
+
+# Private function that renames inputs
+renameControls <- function(controls, oldnames, newnames) {
+  names(newnames) <- oldnames
+  renameControlsRecursive <- function(x) {
+    names(x)[names(x) %in% oldnames] <- newnames[names(x)[names(x) %in% oldnames]]
+    for (n in names(x)) {
+      if (is.list(x[[n]])) {
+        x[[n]] <- renameControlsRecursive(x[[n]])
+      }
+    }
+    return(x)
+  }
+  renameControlsRecursive(controls)
+}
+
+# Private function that resets the initial values of some controls
+resetInitValues <- function(controls, values) {
+  resetInitValuesRecursive <- function(x) {
+    for (n in names(x)) {
+      if (is.list(x[[n]])) {
+        x[[n]] <- resetInitValuesRecursive(x[[n]])
+      } else {
+        if (n %in% names(values) && ! is.null(values[[n]])) {
+          attr(x[[n]], "value") <- values[[n]]
+        }
+      }
+    }
+    return(x)
+  }
+  resetInitValuesRecursive(controls)
+}
+
+# Private function that returns a list woth three elements:
+# - common: list of common controls
+# - ind: list of individual controls for the first chart to compare
+# - ind2: list of individual controls for the seconde chart to compare
+comparisonControls <- function(controls, compare) {
+  common <- filterControls(controls, names(compare), drop = TRUE)
+  ind <- filterControls(controls, names(compare))
+  ind2 <- ind
+
+  # extract the initial values of the individual parameters of each chart
+  initValues1 <- lapply(compare, function(x) {if(is.null(x)) x else x[[1]]})
+  initValues2 <- lapply(compare, function(x) {if(is.null(x)) x else x[[2]]})
+
+  # Reset initial values of input controls
+  ind <- resetInitValues(ind, initValues1)
+  ind2 <- resetInitValues(ind2, initValues2)
+
+  # Add a "2" at the end of the names of the inputs of the second chart
+  ind2 <- renameControls(ind2, names(compare), paste0(names(compare), 2))
+
+  list(common = common, ind = ind, ind2 = ind2)
 }
