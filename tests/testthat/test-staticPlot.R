24a4e342f972170f49103fdc35385a00c21fd66b
Benoit Thieurmel <bthieurmel@gmail.com>
2017-10-25 14:14:10 +0200

    fix staticImage test

--- tests/testthat/test-staticPlot.R
+++ tests/testthat/test-staticPlot.R
@@ -2,9 +2,15 @@ context("Static plot & image")
 
 describe("Static plot & image", {
   it("returns a combineWidget with both static plot and image", {
+
+    tmp_png <- tempfile(fileext = ".png")
+    png(file = tmp_png, bg = "transparent")
+    plot(1:10)
+    dev.off()
+
     c <- combineWidgets(
       staticPlot(hist(iris$Sepal.Length, breaks = 20), height = 300),
-      staticImage(system.file("img/logo.png", package = "manipulateWidget"))
+      staticImage(tmp_png)
     )
 
     expect_is(c, "combineWidgets")
