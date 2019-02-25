203a437a60c5f4d6694c6615816dac01fb261051
cuche27 <guillem_francois@yahoo.fr>
2019-02-25 23:04:28 +0100

    shiny grid module

---
+++ R/grid_module.R
@@ -0,0 +1,22 @@
+gridModuleUI <- function(id) {
+  ns <- NS(id)
+
+  uiOutput(ns("cells"), container = function(...) {div(style = "height:100%;width:100%;position:absolute;", ...)})
+}
+
+gridModuleServer <- function(input, output, session, content, nrow, ncol, ...) {
+  dim <- reactive(.getRowAndCols(length(content()), nrow, ncol))
+
+  observe({
+    outputEls <- lapply(seq_len(dim()$nrow * dim()$ncol), function(i) {
+      if (i > length(content())) return(tags$div())
+      style <- sprintf("float:left;width:%s%%;height:%s%%;border:solid 1px black;",
+                       floor(100 / dim()$ncol), floor(100 / dim()$nrow))
+      tags$div(class="mw-chart", content()[[i]], style = style)
+    })
+
+    output$cells <- renderUI(shiny::tagList(outputEls))
+  })
+
+  return(dim)
+}
