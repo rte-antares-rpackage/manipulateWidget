89ded0f77183f9ab39204a05e5566218cc8ebfb2
cuche27 <guillem_francois@yahoo.fr>
2019-06-30 13:00:40 +0200

    Correct a few problems with worldEnergyUse

--- data-raw/world_energy_use.R
+++ data-raw/world_energy_use.R
@@ -12,11 +12,12 @@ indicators <- c(
   energy_fossil_prop = "EG.USE.COMM.FO.ZS"
 )
 
-data_source <- wb("countries_only", indicators, return_wide = TRUE)
+data_source <- wb("countries_only", indicators, return_wide = TRUE, )
 
 worldEnergyUse <- data_source %>%
   select_at(c("country", "iso2c", year = "date", indicators)) %>%
   mutate(
+    country = as.character(country),
     year = as.numeric(year),
     energy_fossil_prop = energy_fossil_prop / 100,
     energy_imported_prop = energy_imported_prop / 100
@@ -34,9 +35,27 @@ worldEnergyUse <- data_source %>%
 
 # Add country region, longitude and lattitude
 countries <- new_cache$countries %>%
-  select(iso2c, long, lat, region)
+  select(iso2c, long, lat, region) %>%
+  mutate(
+    region = as.character(region),
+    long = as.numeric(long),
+    lat = as.numeric(lat)
+  ) %>%
+  filter(!is.na(long))
 
 worldEnergyUse <- worldEnergyUse %>%
   inner_join(countries, by = "iso2c")
+
+# Remove countries that have only NA values
+countries_to_keep <- worldEnergyUse %>%
+  group_by(iso2c) %>%
+  summarize(not_na = sum(!is.na(energy_used))) %>%
+  filter(not_na > 0) %>%
+  pull(iso2c)
+
+worldEnergyUse <- worldEnergyUse %>%
+  filter(iso2c %in% countries_to_keep)
+
+worldEnergyUse <- as.data.frame(worldEnergyUse)
 
 usethis::use_data(worldEnergyUse, overwrite = TRUE)
